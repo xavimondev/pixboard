c10a9e0994fb615601d522992d076d032a484d51
Xavier Alfaro <nxb.developer@gmail.com>
2023-02-22 09:00:05 -0500

    update bg size

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,7 +6,7 @@ type LayoutProps = {
 
 export function Layout({ children }: LayoutProps) {
   return (
-    <main className='min-h-screen h-full w-full bg-[radial-gradient(#ffffff14_-1px,rgba(0,0,0,0.9)_1px)] bg-[length:20px_20px] p-6 sm:p-8'>
+    <main className='min-h-screen h-full w-full bg-[radial-gradient(#ffffff14_-1px,rgba(0,0,0,0.9)_1px)] bg-[length:24px_24px] p-6 sm:p-8'>
       {children}
     </main>
   )
