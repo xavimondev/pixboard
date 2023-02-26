7a08f4a98d26fdaa832e01b41fdeb4769ae1086f
Xavier Alfaro <nxb.developer@gmail.com>
2023-02-26 15:59:55 -0500

    update padding bottom

--- src/components/header-toolbar.tsx
+++ src/components/header-toolbar.tsx
@@ -2,7 +2,7 @@ import { AvatarsBar } from './avatars-bar'
 
 export function HeaderToolbar() {
   return (
-    <header className='flex flex-row justify-between items-center'>
+    <header className='flex flex-row justify-between items-center mb-6'>
       <div className='bg-neutral-800 p-2.5 px-7 rounded-full flex text-white shadow-sm'>
         <span className='font-semibold text-lg'>Pixboard.io</span>
       </div>
