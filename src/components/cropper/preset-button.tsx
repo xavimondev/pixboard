fd78dd28e1b3eb3b1a1fe8c62878554cffadfd3a
Xavier Alfaro <nxb.developer@gmail.com>
2023-02-26 14:23:33 -0500

    add button presets

---
+++ src/components/cropper/preset-button.tsx
@@ -0,0 +1,44 @@
+import { PRESETS } from '@/utils/constants'
+import React from 'react'
+
+type PresetButtonProps = {
+  onClick: () => void
+  bgClass: string
+  children: React.ReactNode
+}
+
+function PresetButton({ onClick, bgClass, children }: PresetButtonProps) {
+  return (
+    <button
+      className={`${bgClass} shadow-md px-5 py-1.5 rounded-3xl font-semibold`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  )
+}
+
+type ListPresetButtonProps = {
+  handlePreset: (presetId: string) => void
+  presetSelected: string
+}
+
+export function PresetButtons({ handlePreset, presetSelected }: ListPresetButtonProps) {
+  return (
+    <div className='flex gap-3 flex-wrap'>
+      {PRESETS.map((preset) => (
+        <PresetButton
+          key={preset.id}
+          onClick={() => handlePreset(preset.id)}
+          bgClass={
+            presetSelected === preset.id
+              ? 'bg-sky-700/[0.5] text-sky-500'
+              : 'bg-neutral-800 text-white hover:bg-neutral-700'
+          }
+        >
+          {preset.name}
+        </PresetButton>
+      ))}
+    </div>
+  )
+}
---
+++ src/utils/constants.ts
@@ -0,0 +1,32 @@
+export const PRESETS = [
+  {
+    id: 'original',
+    name: 'Original',
+    value: 1
+  },
+  {
+    id: 'pres11',
+    name: 'Square 1:1',
+    value: 1 / 1
+  },
+  {
+    id: 'landscape169',
+    name: 'Landscape 16:9',
+    value: 16 / 9
+  },
+  {
+    id: 'landscape43',
+    name: 'Landscape 4:3',
+    value: 4 / 3
+  },
+  {
+    id: 'port34',
+    name: 'Portrait 3:4',
+    value: 3 / 4
+  },
+  {
+    id: 'port916',
+    name: 'Portrait 9:16',
+    value: 9 / 16
+  }
+]
