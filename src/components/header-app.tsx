3011cb047da4d065fdc31ea92a4cc105983c9138
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-01 17:45:27 -0500

    update component's name

--- src/components/header-app.tsx
+++ src/components/header-app.tsx
@@ -1,6 +1,6 @@
 import { AvatarsBar } from './avatars-bar'
 
-export function HeaderToolbar() {
+export function HeaderApp() {
   return (
     <header className='flex flex-row justify-between items-center mb-6'>
       <div className='bg-neutral-800 p-2.5 px-7 rounded-full flex text-white shadow-sm'>
