b3b7aa3f43f9b87a92761cdbea0f02c7d8f1a450
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-01 23:34:31 -0500

    refactor dropzone by adding new component

--- src/components/dropzone/index.tsx
+++ src/components/dropzone/index.tsx
@@ -3,6 +3,18 @@ import { useDropzone } from 'react-dropzone'
 import { uploadFile } from '@/utils/uploadFile'
 import { AddImageIc } from '@/components/icons'
 
+function DropzoneBody() {
+  return (
+    <div className='flex flex-col space-y-6 items-center'>
+      <AddImageIc className='text-gray-300 h-48 w-48' />
+      <span className='uppercase text-5xl font-medium text-gray-400'>Drag & Drop</span>
+      <p className='text-center text-base text-gray-500'>
+        your images here, or click to select files
+      </p>
+    </div>
+  )
+}
+
 export function Dropzone() {
   const [isUploading, setIsUploading] = useState<boolean>(false)
   const onDrop = useCallback(async (files: File[]) => {
@@ -29,17 +41,7 @@ export function Dropzone() {
       className='flex items-center justify-center mx-auto max-w-3xl border bg-neutral-800 border-gray-700 overflow-hidden h-[500px] z-0 rounded-xl shadow-sm hover:opacity-90 hover:border-dashed hover:border-2 hover:border-gray-400 hover:cursor-pointer'
     >
       <input {...getInputProps()} />
-      {isUploading ? (
-        <h2>Loading</h2>
-      ) : (
-        <div className='flex flex-col space-y-6 items-center'>
-          <AddImageIc className='text-gray-300 h-48 w-48' />
-          <span className='uppercase text-5xl font-medium text-gray-400'>Drag & Drop</span>
-          <p className='text-center text-base text-gray-500'>
-            your images here, or click to select files
-          </p>
-        </div>
-      )}
+      {isUploading ? <h2>Loading</h2> : <DropzoneBody />}
     </div>
   )
 }
