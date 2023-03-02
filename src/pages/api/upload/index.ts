17620b9e5aa6d3cb8f94e31d3fc4067f942adbc1
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-01 23:14:10 -0500

    add api to upload files on cloudinary

---
+++ src/pages/api/upload/index.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import cloudinary from 'cloudinary'
+
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const image = req.body.imageBase64
+    const uploadedImageResponse = await cloudinary.v2.uploader.upload(image)
+    console.log(uploadedImageResponse)
+    return res.status(200).json({
+      url: uploadedImageResponse.secure_url
+    })
+  } catch (error) {
+    return res.status(500).json({
+      error: 'An error has ocurred'
+    })
+  }
+}
