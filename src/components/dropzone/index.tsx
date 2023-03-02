6f369d29f727d5bd2a3ab20f0fccfb65fc590cca
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-01 23:15:51 -0500

    add dropzone uploads files to cloudinary

--- src/components/dropzone/index.tsx
+++ src/components/dropzone/index.tsx
@@ -1,14 +1,21 @@
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
-import { AddImageIc } from '../icons'
+import { uploadFile } from '@/utils/uploadFile'
+import { AddImageIc } from '@/components/icons'
 
 export function Dropzone() {
-  const onDrop = useCallback((files: File[]) => {
-    console.log(files)
+  const [isUploading, setIsUploading] = useState<boolean>(false)
+  const onDrop = useCallback(async (files: File[]) => {
+    const file = files[0]
+    setIsUploading(true)
+    const url = await uploadFile(file)
+    console.log(url)
+    setIsUploading(false)
   }, [])
 
   const { getRootProps, getInputProps } = useDropzone({
     maxFiles: 1,
+    maxSize: 20971520, // 21MB
     accept: {
       'image/*': []
     },
@@ -22,13 +29,17 @@ export function Dropzone() {
       className='flex items-center justify-center mx-auto max-w-3xl border bg-neutral-800 border-gray-700 overflow-hidden h-[500px] z-0 rounded-xl shadow-sm hover:opacity-90 hover:border-dashed hover:border-2 hover:border-gray-400 hover:cursor-pointer'
     >
       <input {...getInputProps()} />
-      <div className='flex flex-col space-y-6 items-center'>
-        <AddImageIc className='text-gray-300 h-48 w-48' />
-        <span className='uppercase text-5xl font-medium text-gray-400'>Drag & Drop</span>
-        <p className='text-center text-base text-gray-500'>
-          your images here, or click to select files
-        </p>
-      </div>
+      {isUploading ? (
+        <h2>Loading</h2>
+      ) : (
+        <div className='flex flex-col space-y-6 items-center'>
+          <AddImageIc className='text-gray-300 h-48 w-48' />
+          <span className='uppercase text-5xl font-medium text-gray-400'>Drag & Drop</span>
+          <p className='text-center text-base text-gray-500'>
+            your images here, or click to select files
+          </p>
+        </div>
+      )}
     </div>
   )
 }
