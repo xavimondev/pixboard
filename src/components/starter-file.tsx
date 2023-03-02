1bc9d90957d8ae3f3fb273122dac8751c4932d5b
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-02 10:35:42 -0500

    update styles

--- src/components/starter-file.tsx
+++ src/components/starter-file.tsx
@@ -5,7 +5,7 @@ type StarterFileProps = {
 }
 export function StarterFile({ children }: StarterFileProps) {
   return (
-    <div className='flex flex-col justify-center mx-auto max-w-3xl border bg-neutral-800 border-gray-700 overflow-hidden max-h-[700px] rounded-xl shadow-sm p-2'>
+    <div className='mt-16 flex flex-col justify-center mx-auto max-w-3xl bg-neutral-800 overflow-hidden max-h-[700px] rounded-xl shadow-sm p-2'>
       {children}
     </div>
   )
