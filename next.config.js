8ca56402dbc4a6b76d6d76217e4b1fb0d0efef2f
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-04 16:00:29 -0500

    add url to next conf

--- next.config.js
+++ next.config.js
@@ -14,6 +14,10 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'cloudflare-ipfs.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com'
       }
     ]
   }
