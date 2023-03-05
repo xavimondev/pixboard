2ec5fa1911468d49d0ed4e65a2dede1e33df88a2
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-05 15:24:23 -0500

    fix wrong props show error on console

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -36,8 +36,8 @@ export function UserIc(props: SVGProps<SVGSVGElement>) {
         d='m7.5.5c1.65685425 0 3 1.34314575 3 3v2c0 1.65685425-1.34314575 3-3 3s-3-1.34314575-3-3v-2c0-1.65685425 1.34314575-3 3-3zm7 14v-.7281753c0-3.1864098-3.6862915-5.2718247-7-5.2718247s-7 2.0854149-7 5.2718247v.7281753c0 .5522847.44771525 1 1 1h12c.5522847 0 1-.4477153 1-1z'
         fill='none'
         stroke='currentColor'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeLinecap='round'
+        strokeLinejoin='round'
         transform='translate(3 2)'
       />
     </svg>
@@ -326,7 +326,7 @@ export function ShareIc(props: SVGProps<SVGSVGElement>) {
 export function UndoAllIc(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-      stroke-width='1.5'
+      strokeWidth='1.5'
       viewBox='0 0 24 24'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
