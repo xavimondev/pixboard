80c0e68ac071ca1b6196dbc74fa55735e4787ede
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-04 22:28:50 -0500

    fix 'Component' does not have any construct or call signatures

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -7,15 +7,15 @@ import { Filters } from '@/components/filters'
 const toolsComponents = [
   {
     id: 'crop',
-    Component: Cropper
+    Component: <Cropper />
   },
   {
     id: 'overlay',
-    Component: TextOverlay
+    Component: <TextOverlay />
   },
   {
     id: 'effects',
-    Component: Filters
+    Component: <Filters />
   }
 ]
 
@@ -24,14 +24,14 @@ export function Editor() {
   const toolSelected = useStore((state) => state.toolSelected)
   const mainImage = useStore((state) => state.mainImage)
   if (!mainImage) return null
-  const Component = toolsComponents.find((tool) => tool.id === toolSelected)?.Component
+  const Component = toolsComponents.find((tool) => tool.id === toolSelected)!.Component
   return (
     <>
       <section
         className='mt-10 flex flex-col sm:flex-row w-full space-y-3 sm:space-y-0 sm:space-x-4 p-10'
         onPointerMove={(e) => setCursor({ x: e.clientX, y: e.clientY })}
       >
-        <Component />
+        {Component}
         <ListCursors />
       </section>
     </>
