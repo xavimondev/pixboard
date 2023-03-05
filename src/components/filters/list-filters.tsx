58323c183ef75e94673dd3842be685b587226c40
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-05 15:27:26 -0500

    fix error message

--- src/components/filters/list-filters.tsx
+++ src/components/filters/list-filters.tsx
@@ -54,7 +54,7 @@ const FilterOption = React.memo(function FilterOption({ filter }: FilterOptionPr
       url
     })
   }
-
+  // TODO: Change ts ignore
   return (
     <button
       onClick={applyFilter}
@@ -64,6 +64,7 @@ const FilterOption = React.memo(function FilterOption({ filter }: FilterOptionPr
         width={100}
         height={100}
         alt={`Effect ${filter}`}
+        // @ts-ignore
         sepia={filter === 'sepia'}
         grayscale={filter === 'grayscale'}
         effects={othersFilters}
