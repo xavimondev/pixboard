e33af9a7d11aeb5ef64b421c06071563355be1ff
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-06 12:42:47 -0500

    add loader indicator while image is loading

--- src/components/download/index.tsx
+++ src/components/download/index.tsx
@@ -1,25 +1,38 @@
 import Image from 'next/image'
 import useStore from '@/state/store'
 import { getImageScale } from '@/utils/getScale'
+import { ImageLoader } from '@/components/loaders'
 import { ListTools } from './list-tools'
 
 export function Download() {
   const imageTransformedData = useStore((state) => state.imageTransformedData)
   const { url, width, height } = imageTransformedData!
   const { scaleWidth, scaleHight } = getImageScale(width, height)
+  const isLoadingImage = useStore((state) => state.isLoadingImage)
+  const setIsLoadingImage = useStore((state) => state.setIsLoadingImage)
 
   return (
     <>
-      <div className='flex flex-row justify-between w-full space-x-4'>
+      <div className='flex flex-row justify-between items-center gap-6 w-full h-full'>
         <ListTools />
-        <div className='min-h-[400px] min-w-[600px] max-h-[600px] max-w-[800px] flex justify-center items-center bg-default-image border border-neutral-700'>
+        <div
+          className={`max-h-[600px] max-w-[800px] flex justify-center items-center ${
+            !isLoadingImage ? 'border border-neutral-700' : ''
+          }`}
+        >
           <Image
             alt='Imagen choosen by user'
             src={url}
             width={scaleWidth}
             height={scaleHight}
             className='max-h-full max-w-full object-cover'
+            onLoadingComplete={() => {
+              setIsLoadingImage(false)
+            }}
           />
+          <div className={`${isLoadingImage ? 'block w-3/12' : 'hidden'}`}>
+            <ImageLoader />
+          </div>
         </div>
       </div>
     </>
--- src/components/filters/index.tsx
+++ src/components/filters/index.tsx
@@ -2,23 +2,34 @@ import Image from 'next/image'
 import useStore from '@/state/store'
 import { getImageScale } from '@/utils/getScale'
 import { ListFilters } from './list-filters'
+import { ImageLoader } from '@/components/loaders'
 
 export function Filters() {
   const imageTransformedData = useStore((state) => state.imageTransformedData)
   const { url, width, height } = imageTransformedData!
   const { scaleWidth, scaleHight } = getImageScale(width, height)
+  const isLoadingImage = useStore((state) => state.isLoadingImage)
+  const setIsLoadingImage = useStore((state) => state.setIsLoadingImage)
 
   return (
     <>
-      <div className='flex flex-row justify-between w-full space-x-4'>
+      <div className='flex flex-row items-center w-full space-x-4'>
         <ListFilters />
-        <div className='min-h-[400px] min-w-[600px] max-h-[600px] max-w-[800px] flex justify-center items-center bg-default-image border border-neutral-700'>
+        <div
+          className={`max-h-[600px] max-w-[800px] flex justify-center items-center ${
+            !isLoadingImage ? 'border border-neutral-700' : ''
+          }`}
+        >
+          {isLoadingImage && <ImageLoader />}
           <Image
             alt='Imagen choosen by user'
             src={url}
             width={scaleWidth}
             height={scaleHight}
             className='max-h-full max-w-full object-cover'
+            onLoadingComplete={() => {
+              setIsLoadingImage(false)
+            }}
           />
         </div>
       </div>
