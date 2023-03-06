1fba10930c2ac97d95601b7fe9ef32f9f6c078ce
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-06 12:40:27 -0500

    update using new state

--- src/components/editor/original-image.tsx
+++ src/components/editor/original-image.tsx
@@ -1,16 +1,17 @@
-import React, { useState } from 'react'
-import { CldImage } from 'next-cloudinary'
+import React from 'react'
+import Image from 'next/image'
 import useStore from '@/state/store'
 
 export const OriginalImage = React.memo(function OriginalImage() {
-  const [isLoading, setLoading] = useState(true)
   const mainImage = useStore((state) => state.mainImage)
   const setMainImage = useStore((state) => state.setMainImage)
+  const isLoading = useStore((state) => state.isLoading)
+  const setIsLoading = useStore((state) => state.setIsLoading)
 
   const { url, width, height } = mainImage!.imageData
 
   return (
-    <CldImage
+    <Image
       alt='Garden weird'
       src={url}
       width={width}
@@ -18,7 +19,7 @@ export const OriginalImage = React.memo(function OriginalImage() {
       className={`max-h-full max-w-full object-cover duration-700 ease-in-out scale-100 ${
         isLoading ? 'grayscale blur-2xl' : 'grayscale-0 blur-0'
       }`}
-      onLoadingComplete={() => setLoading(false)}
+      onLoadingComplete={() => setIsLoading(false)}
       onLoad={(evt: React.SyntheticEvent<HTMLImageElement>) => {
         const { width, height } = evt.currentTarget
         if (mainImage) {
