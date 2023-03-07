8d7a69aa68c8dd6295bbbb452d5893806cdc96cf
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-06 22:12:13 -0500

    Add button to go to new room

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -488,3 +488,24 @@ export function AddIc(props: SVGProps<SVGSVGElement>) {
     </svg>
   )
 }
+
+export function ArrowRightIc(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      strokeWidth='1.8'
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      color='#000000'
+      {...props}
+    >
+      <path
+        d='M6 12h12.5m0 0l-6-6m6 6l-6 6'
+        stroke='currentColor'
+        strokeWidth='1.8'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { GetServerSideProps } from 'next'
 import { getServerSession } from 'next-auth'
-import { User } from '@/types/user'
 import { authOptions } from './api/auth/[...nextauth]'
+import { User } from '@/types/user'
+import { getBoardId } from '@/utils/getRandomData'
 import { Layout } from '@/components/layout'
+import { ArrowRightIc } from '@/components/icons'
 
 type BoardProps = {
   userInfo: {
@@ -14,7 +16,19 @@ type BoardProps = {
 export default function Home({ userInfo }: BoardProps) {
   return (
     <>
-      <Layout user={userInfo.user} isToolbarEnable />
+      <Layout user={userInfo.user} isToolbarEnable>
+        {userInfo.user ? (
+          <div className='flex items-center justify-center'>
+            <a
+              className='text-sky-500 font-semibold text-base bg-sky-700/[0.5] py-1.5 px-4 rounded-md flex items-center gap-2'
+              href={`/board/${getBoardId()}`}
+            >
+              Go to Room
+              <ArrowRightIc className='w-5 h-5 text-sky-500' />
+            </a>
+          </div>
+        ) : null}
+      </Layout>
     </>
   )
 }
