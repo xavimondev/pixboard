87cf433bc4c3964dea7c9d630353a11b3ee6a108
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-07 00:06:49 -0500

    add pixboard logo is clickeable

--- src/components/header-app.tsx
+++ src/components/header-app.tsx
@@ -1,12 +1,15 @@
+import Link from 'next/link'
 import { RightHeaderBar } from './right-bar'
 import { Toolbar } from './toolbar'
 
 export function HeaderApp({ isToolbarEnable }: { isToolbarEnable: boolean }) {
   return (
     <header className='flex flex-row justify-between items-center mb-6'>
-      <div className='bg-neutral-800 py-2.5 px-6 rounded-full flex text-white shadow-sm'>
-        <span className='font-semibold text-base'>Pixboard.io</span>
-      </div>
+      <Link className='font-semibold text-base' href='/'>
+        <div className='bg-neutral-800 py-2.5 px-6 rounded-full flex text-white shadow-sm'>
+          Pixboard.io
+        </div>
+      </Link>
       {isToolbarEnable && (
         <>
           <Toolbar />
